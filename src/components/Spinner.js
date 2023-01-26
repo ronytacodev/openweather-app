56ac80f1c54c9144a753b57ee6c68093f148e17f
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-26 11:37:21 -0500

    creating component Spinner js and css

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../assets/css/Spinner.css";
+
+const Spinner = () => {
+    return (
+        <div class="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Spinner;
