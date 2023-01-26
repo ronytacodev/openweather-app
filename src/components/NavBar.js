9c6fe3ac000b4f83a3665977dfa3b4b8294fe38e
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-25 22:08:55 -0500

    comenzando el proyecto del app del clima

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar bg-dark text-light mb-5">
+            <div className="container-fluid">
+                <h3 className="mx-auto">Predicción del clima</h3>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
